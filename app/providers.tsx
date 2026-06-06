be5752fcbdace362d59a5540155545e8eed45c19
Thomas LY <lythomm@gmail.com>
2026-06-06 18:15:19 +0200

    remove console logs

--- app/providers.tsx
+++ app/providers.tsx
@@ -21,12 +21,27 @@ function PostHogPageView() {
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    if (pathname && posthog.__loaded) {
+    const key = process.env.NEXT_PUBLIC_POSTHOG_KEY || process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN;
+    const host = process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com";
+    if (typeof window !== "undefined" && key) {
+      posthog.init(key, {
+        api_host: host,
+        person_profiles: "identified_only",
+        capture_pageview: false, // Tracked manually below to capture SPA route changes
+        capture_heatmaps: true,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (pathname && typeof window !== "undefined") {
       let url = window.origin + pathname;
       if (searchParams && searchParams.toString()) {
         url = url + `?${searchParams.toString()}`;
       }
-      posthog.capture("$pageview", { $current_url: url });
+      posthog.capture("$pageview", {
+        $current_url: url,
+      });
     }
   }, [pathname, searchParams]);
 
